1331023e88e7917a38bab925d7d4513390af0f60
Alexandre Filho <alexfiofcasf@gmail.com>
2017-11-04 11:56:51 -0300

    testando

--- public/deploy.php
+++ public/deploy.php
@@ -22,7 +22,7 @@ if ($payload = file_get_contents('php://input')) {
     }
     $branch = \explode('/',$payload->ref)[2];
     //put the branch you want here, as well as the directory your site is in
-    echo "teste";
+    echo "teste - cd /var/www/html/espaco && git fetch origin && git merge origin/$branch";
     echo exec("cd /var/www/html/espaco && git fetch origin && git merge origin/$branch");
 
 } else {
