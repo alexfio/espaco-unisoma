df299a3536977322bc56902f38274f2776dba50d
Alexandre Filho <alexfiofcasf@gmail.com>
2017-11-04 11:37:27 -0300

    alterações

--- public/deploy.php
+++ public/deploy.php
@@ -1,5 +1,27 @@
 <?php
+// webhook do mgs !
+// verifica se a requisição vem do servidor gitlab
+//$gitlab_ips = array('172.31.3.8', '127.0.0.1');
+//if (!in_array($_SERVER['REMOTE_ADDR'], $gitlab_ips)) {
+//    throw new Exception("Isto não parece uma requisição válida do Gitlab.\n");/
+//}
 
-echo trim(file_get_contents('php://input'));
-//$input_data = json_decode(trim(file_get_contents('php://input')), true);
+if ($payload = file_get_contents('php://input')) {
+    try {
+        $payload = json_decode($payload);
+    } catch (Exception $ex) {
+        echo $ex;
+        exit(0);
+    }
+    // put the branch you want here
+    if (!\in_array($payload->ref, ['refs/heads/master', 'refs/heads/develop'])) {
+        echo 'cabeçalho errado';
+        exit(0);
+    }
+    $branch = \explode('/',$payload->ref)[2];
+    //put the branch you want here, as well as the directory your site is in
+    echo exec("cd /var/www/html/espaco && git fetch origin && git merge origin/$branch");
 
+} else {
+    echo 'requisição falhou';
+}
